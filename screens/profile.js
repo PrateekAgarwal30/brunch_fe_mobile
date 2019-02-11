af772b38077237b4f9b91813f030fd6ff0a4d02f
Prateek Agarwal <prateek.agarwal.30@gmail.com>
2019-02-12 01:55:41 +0530

    Redesigning Profile Screen

--- screens/profile.js
+++ screens/profile.js
@@ -1,9 +1,31 @@
 import React from "react";
-import { Text, View, TouchableHighlight,TouchableOpacity,FlatList } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TouchableHighlight,
+  TouchableOpacity,
+  ScrollView,
+  FlatList,
+  Button
+} from "react-native";
 import { connect } from "react-redux";
 import { getProfile } from "../redux/actions";
-import { Button, List, ListItem } from "native-base";
+import Separator from "./../components/Separator";
+import { Icon } from "native-base";
+
 class Profile extends React.Component {
+  static navigationOptions = {
+    headerTitle: "Profile",
+    headerStyle: {
+      backgroundColor: "#16235A"
+    },
+    headerTintColor: "#fff",
+    headerTitleStyle: {
+      fontWeight: "bold"
+    }
+  };
   state = {
     addresses: []
   };
@@ -20,37 +42,120 @@ class Profile extends React.Component {
         console.log(err.message);
       });
   }
-  _onPressButton = ({item}) => {
-    console.log(item);
-    this.props.navigation.navigate("Address");
-  }
-  _renderItem = ({item})=>{
-    return <Text onPress={this._onPressButton}>{JSON.stringify(item)}</Text>
-  }
-  _keyExtractor = (item,index)=> item._id;
   render() {
     return (
-      <View>
-        <Text>{JSON.stringify(this.state)}</Text>
-        <Button onPress={() => this.props.navigation.navigate("Address")}>
-          <Text>Address</Text>
-        </Button>
-        <List>
-          {this.state.addresses.map(m => {
-            return (
-              <FlatList
-                data={this.state.addresses}
-                renderItem={this._renderItem}
-                keyExtractor = {this._keyExtractor}
-              />
-            );
-          })}
-        </List>
+      <View style={styles.container}>
+        <View style={styles.header} />
+        <Image
+          style={styles.avatar}
+          source={require("./../assets/male-avatar.png")}
+        />
+        <View style={styles.body}>
+          <View style={styles.bodyContent}>
+            <Text style={styles.name}>{this.props.profile.email}</Text>
+            <Text style={styles.info}>S</Text>
+          </View>
+          <ScrollView style={styles.buttonContainer}>
+            <TouchableHighlight style={styles.buttonWrapper} onPress={() => alert("Clicked")}>
+              <View style={styles.buttonInsideView}>
+                <Icon name="person" style={{ flex: 1 }} />
+                <Text style={styles.textWrapper}>Personal Info</Text>
+                <Icon name="ios-arrow-forward" style={{ flex: 1 }} />
+              </View>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.buttonWrapper} onPress={() => alert("Clicked")}>
+              <View style={styles.buttonInsideView}>
+                <Icon name="map" style={{ flex: 1 }} />
+                <Text style={styles.textWrapper}>Manage Addresses</Text>
+                <Icon name="ios-arrow-forward" style={{ flex: 1 }} />
+              </View>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.buttonWrapper} onPress={() => alert("Clicked")}>
+              <View style={styles.buttonInsideView}>
+                <Icon name="heart" style={{ flex: 1 }} />
+                <Text style={styles.textWrapper}>Favourite</Text>
+                <Icon name="ios-arrow-forward" style={{ flex: 1 }} />
+              </View>
+            </TouchableHighlight>
+            <TouchableOpacity style={styles.buttonWrapper} onPress={()=>alert("Clicked")}>
+              <View style={styles.buttonInsideView}>
+                <Icon name="keypad" style={{ flex: 1 }} />
+                <Text style={styles.textWrapper}>Change Password</Text>
+                <Icon name="ios-arrow-forward" style={{ flex: 1 }} />
+              </View>
+            </TouchableOpacity>
+          </ScrollView>
+        </View>
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#16235A",
+    height: 100
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: "white",
+    marginBottom: 10,
+    alignSelf: "center",
+    position: "absolute",
+    marginTop: 40
+  },
+  name: {
+    fontSize: 22,
+    color: "#FFFFFF",
+    fontWeight: "600"
+  },
+  body: {
+    marginTop: 20
+  },
+  bodyContent: {
+    flex: 1,
+    alignItems: "center",
+    padding: 30
+  },
+  name: {
+    fontSize: 28,
+    color: "#696969",
+    fontWeight: "600"
+  },
+  info: {
+    fontSize: 16,
+    color: "#00BFFF",
+    marginTop: 10
+  },
+  description: {
+    fontSize: 16,
+    color: "#696969",
+    marginTop: 10,
+    textAlign: "center"
+  },
+  buttonContainer: {
+    marginTop: 20
+  },
+  textWrapper: {
+    fontSize: 18,
+    // padding : 20,
+    // textAlign: 'center',
+    marginLeft : 5,
+    flex : 10
+  },
+  buttonWrapper: {
+    marginBottom: 20,
+    backgroundColor : '#A4A4BF'
+  },
+  buttonInsideView : {
+    flex: 1,
+    flexDirection: 'row',
+    padding : 10
+  }
+});
 const mapStateToProps = state => ({
   user: state.user,
   profile: state.profile
