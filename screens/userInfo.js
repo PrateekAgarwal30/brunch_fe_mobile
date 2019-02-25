58e4e1e9b17003ec5dafba403981da43157ca800
Prateek Agarwal <prateek.agarwal.30@gmail.com>
2019-02-25 23:50:48 +0530

    Working on UserInfo:edit profile

--- screens/userInfo.js
+++ screens/userInfo.js
@@ -5,21 +5,50 @@ import {
 } from "react-native";
 import { connect } from "react-redux";
 // import { Separator } from "native-base";
-import { Container, Header, Content, Form, Item, Input, Label, Separator } from 'native-base';
+import { Container, Header, Content, Form, Item, Input, Label, Separator, Button } from 'native-base';
 class UserInfo extends React.Component {
   state = {
-    firstName: null,
-    lastName: null,
-    email : null,
-    phoneNumber: null,
-    location: null,
-    dateOfBirth: null,
-    preferredMeal: null,
-    description: null
+    db: {
+      firstName: null,
+      lastName: null,
+      email: null,
+      phoneNumber: null,
+      location: null,
+      dateOfBirth: null,
+      preferredMeal: null,
+      description: null,
+      editable: false
+    },
+    edit: {
+      firstName: null,
+      lastName: null,
+      email: null,
+      phoneNumber: null,
+      location: null,
+      dateOfBirth: null,
+      preferredMeal: null,
+      description: null,
+    },
+    editing: false
   }
   componentDidMount() {
+    if (this.state.editing) {
+      this.setState({
+        ...this.state,
+        db: { ...this.props.profile.details }
+      })
+    } else {
+      this.setState({
+        ...this.state,
+        db: { ...this.props.profile.details },
+        edit: { ...this.props.profile.details }
+      })
+    }
+  }
+  _editableToggle = () => {
     this.setState({
-      ...this.props.profile.details
+      ...this.state,
+      editing: !this.state.editing
     })
   }
   render() {
@@ -28,27 +57,45 @@ class UserInfo extends React.Component {
         <Content>
           <Form>
             <Item style={{ flexDirection: 'row' }}>
-              <Item stackedLabel style={{flex:1}}>
+              <Item stackedLabel style={{ flex: 1 }}>
                 <Label>First Name</Label>
-                <Input value={this.state.firstName}/>
+                <Input value={this.state.editing ? this.state.edit.firstName : this.state.db.firstName} editable={this.state.editing} />
               </Item>
               <Item stackedLabel style={{ flex: 1 }}>
                 <Label>Last Name</Label>
-                <Input value={this.state.lastName} />
+                <Input value={this.state.editing ? this.state.edit.lastName : this.state.db.lastName} editable={this.state.editing} />
               </Item>
             </Item>
             <Item stackedLabel>
               <Label>Email</Label>
-              <Input value={this.state.email} editable={false} />
+              <Input value={this.state.db.email} />
             </Item>
             <Item stackedLabel>
-              <Label>Password</Label>
-              <Input value={"1"} />
+              <Label>Mobile Number</Label>
+              <Input value={this.state.editing ? this.state.edit.phoneNumber : this.state.db.phoneNumber} editable={this.state.editing} />
+            </Item>
+            <Item stackedLabel>
+              <Label>Location</Label>
+              <Input value={this.state.editing ? this.state.edit.location : this.state.db.location} editable={this.state.editing} />
+            </Item>
+            <Item stackedLabel>
+              <Label>Date Of Birth</Label>
+              <Input value={this.state.editing ? this.state.edit.dateOfBirth : this.state.db.dateOfBirth} editable={this.state.editing} />
+            </Item>
+            <Item stackedLabel>
+              <Label>Preferred Meal</Label>
+              <Input value={this.state.editing ? this.state.edit.preferredMeal : this.state.db.preferredMeal} editable={this.state.editing} />
+            </Item>
+            <Item stackedLabel>
+              <Label>Description</Label>
+              <Input value={this.state.editing ? this.state.edit.description : this.state.db.description} editable={this.state.editing} />
             </Item>
           </Form>
         </Content>
-        <Text>{JSON.stringify(this.props.user)}</Text>
+        <Button onPress={this._editableToggle}><Text>Edit</Text></Button>
+        {/* <Text>{JSON.stringify(this.props.user)}</Text>
         <Text>{JSON.stringify(this.props.profile)}</Text>
+        <Text>{JSON.stringify(this.state)}</Text> */}
       </View>
     );
   }
