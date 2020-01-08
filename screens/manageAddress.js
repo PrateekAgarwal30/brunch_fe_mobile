d4a60cf5e3494d0115f41dc0a2872c0e1e9f1375
Prateek Agarwal <prateek.agarwal.30@gmail.com>
2020-01-09 01:05:29 +0530

    Bug Fix : Design Screen when no Address is present

--- screens/manageAddress.js
+++ screens/manageAddress.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { View, Text, StyleSheet, Dimensions } from "react-native";
+import { Text, StyleSheet, Dimensions, Image } from "react-native";
 import { connect } from "react-redux";
 import _ from "lodash";
-import { Body, Button, Card, CardItem, Content, Label } from "native-base";
+import {
+  Body,
+  Button,
+  Card,
+  CardItem,
+  Content,
+  Label,
+  Container
+} from "native-base";
 import CustomActivityIndicator from "../components/CustomActivityIndicator";
 import MapView from "react-native-maps";
 const { width } = Dimensions.get("window");
@@ -26,7 +34,14 @@ const styles = StyleSheet.create({
   cardBody: {
     paddingTop: 3,
     paddingBottom: 3
-    // elevation :4
+  },
+  noImageFoundCard: {
+    marginTop: 10,
+    elevation: 3,
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+    minHeight: 300
   }
 });
 
@@ -49,77 +64,96 @@ class ManageAddress extends React.Component {
   render() {
     const address = this.props.profile.addresses;
     const isLoading = this.props.user.isLoading;
-    if (isLoading || !address) {
+    if (isLoading) {
       return <CustomActivityIndicator />;
     }
     return (
-      <View
+      <Container
         style={{
           flex: 1,
-          zIndex: 0,
+          zIndex: 0
         }}
       >
-        <MapView
-          style={styles.map}
-          initialRegion={{
-            ...address.stall_loc_id.location,
-            latitudeDelta: 0.005,
-            longitudeDelta: 0.01121
-          }}
-          showsCompass={true}
-          rotateEnabled={false}
-          showsUserLocation={true}
-          region={{
-            ...address.stall_loc_id.location,
-            latitudeDelta: 0.01121,
-            longitudeDelta: 0.01121
-          }}
-          ref={map => {
-            this.map = map;
-          }}
-          customMapStyle={mapStyle}
-          zoomEnabled={false}
-          scrollEnabled={false}
-        >
-          {address.stall_loc_id.location && (
-            <MapView.Marker
-              // key={x._id}
-              zIndex={9}
-              coordinate={address.stall_loc_id.location}
-              title={`${address.stall_loc_id.tag}, ${address.tech_park_id.techPark}`}
-              pointerEvents="none"
-              onPress={x => {}}
-            ></MapView.Marker>
-          )}
-        </MapView>
-        <Content padder>
-          <Card style={styles.cardContainer}>
-            <CardItem bordered style={styles.cardBody}>
-              <Body>
-                <Label style={styles.labelStyle}>{`Stall : `}</Label>
-                <Text
-                  style={styles.textStyle}
-                >{`${address.stall_loc_id.tag}`}</Text>
-              </Body>
-            </CardItem>
-            <CardItem bordered style={styles.cardBody}>
-              <Body>
-                <Label style={styles.labelStyle}>{`Tech Park : `}</Label>
-                <Text
-                  style={styles.textStyle}
-                >{`${address.tech_park_id.techPark}`}</Text>
-              </Body>
-            </CardItem>
-            <CardItem bordered style={styles.cardBody}>
-              <Body>
-                <Label style={styles.labelStyle}>{`Address : `}</Label>
-                <Text
-                  style={styles.textStyle}
-                >{`${address.tech_park_id.address}, ${address.tech_park_id.area}, ${address.tech_park_id.city}`}</Text>
-              </Body>
-            </CardItem>
-          </Card>
-        </Content>
+        {address ? (
+          <Container>
+            <MapView
+              style={styles.map}
+              initialRegion={{
+                ...address.stall_loc_id.location,
+                latitudeDelta: 0.005,
+                longitudeDelta: 0.01121
+              }}
+              showsCompass={true}
+              rotateEnabled={false}
+              showsUserLocation={true}
+              region={{
+                ...address.stall_loc_id.location,
+                latitudeDelta: 0.01121,
+                longitudeDelta: 0.01121
+              }}
+              ref={map => {
+                this.map = map;
+              }}
+              customMapStyle={mapStyle}
+              zoomEnabled={false}
+              scrollEnabled={false}
+            >
+              {address.stall_loc_id.location && (
+                <MapView.Marker
+                  // key={x._id}
+                  zIndex={9}
+                  coordinate={address.stall_loc_id.location}
+                  title={`${address.stall_loc_id.tag}, ${address.tech_park_id.techPark}`}
+                  pointerEvents="none"
+                  onPress={x => {}}
+                ></MapView.Marker>
+              )}
+            </MapView>
+            <Content padder>
+              <Card style={styles.cardContainer}>
+                <CardItem bordered style={styles.cardBody}>
+                  <Body>
+                    <Label style={styles.labelStyle}>{`Stall : `}</Label>
+                    <Text
+                      style={styles.textStyle}
+                    >{`${address.stall_loc_id.tag}`}</Text>
+                  </Body>
+                </CardItem>
+                <CardItem bordered style={styles.cardBody}>
+                  <Body>
+                    <Label style={styles.labelStyle}>{`Tech Park : `}</Label>
+                    <Text
+                      style={styles.textStyle}
+                    >{`${address.tech_park_id.techPark}`}</Text>
+                  </Body>
+                </CardItem>
+                <CardItem
+                  bordered
+                  style={{ ...styles.cardBody, paddingBottom: 6 }}
+                >
+                  <Body>
+                    <Label style={styles.labelStyle}>{`Address : `}</Label>
+                    <Text
+                      style={styles.textStyle}
+                    >{`${address.tech_park_id.address}, ${address.tech_park_id.area}, ${address.tech_park_id.city}`}</Text>
+                  </Body>
+                </CardItem>
+              </Card>
+            </Content>
+          </Container>
+        ) : (
+          <Container>
+            <Content padder>
+              <Card style={styles.noImageFoundCard}>
+                <Image
+                  style={{ height: 100, width: 100 }}
+                  source={require("./../assets/company.png")}
+                />
+                <Text>{"No Address Found!"}</Text>
+              </Card>
+            </Content>
+          </Container>
+        )}
         <Content
           padder
           style={{ position: "absolute", width: "100%", bottom: 0, left: 0 }}
@@ -132,11 +166,11 @@ class ManageAddress extends React.Component {
             onPress={this._onChangeAddress}
           >
             <Text style={{ color: "white", fontSize: 16 }}>
-              {"Change Address"}
+              {address ? "Change Address" : "Add Address"}
             </Text>
           </Button>
         </Content>
-      </View>
+      </Container>
     );
   }
 }
