8e603d30017ab50109d2e9b932eed72176eee4f2
Prateek Agarwal <prateek.agarwal.30@gmail.com>
2020-02-19 01:25:36 +0530

    Fixing RazorPay Payment Issues

--- components/PaypalPaymentModal.js
+++ components/PaypalPaymentModal.js
@@ -1,61 +1,67 @@
 import React from "react";
-import { Modal, WebView } from "react-native";
+import {Modal, WebView} from "react-native";
 import _ from "lodash";
-import { ipAddress } from "../constants";
+import {ipAddress} from "../constants";
 class PaytmPaymentModal extends React.Component {
-  _onloadingError = x => {
-    console.log("_onloadingError");
-    this.props.toggleModalVisiblity(false);
-  };
-  _handleTransaction = data => {
-    const { loading, url, title } = data;
-    console.log(data);
-    if (
-      loading === false &&
-      _.startsWith(url, `${ipAddress}/api/txn/paypal/status`)
-    ) {
-      const jsonData = JSON.parse(title);
-      console.log("jsonData", jsonData);
-      this.props.toggleModalVisiblity(false);
+    _onloadingError = x => {
+        console.log("_onloadingError");
+        this
+            .props
+            .toggleModalVisiblity(false);
+    };
+    _handleTransaction = data => {
+        const {loading, url, title} = data;
+        console.log(data);
+        if (loading === false && _.startsWith(url, `${ipAddress}/api/txn/paypal/status`)) {
+            const jsonData = JSON.parse(title);
+            console.log("jsonData", jsonData);
+            this
+                .props
+                .toggleModalVisiblity(false);
+            this
+                .props
+                .getProfile();
+        }
+    };
+    _onRequestClose = () => {
+        this
+            .props
+            .toggleModalVisiblity(false);
+    };
+    render() {
+        const {authToken, modalVisible} = this.props;
+        return (
+            <Modal
+                animationType="slide"
+                transparent={false}
+                visible={modalVisible}
+                onRequestClose={this._onRequestClose}
+                onDismiss={() => {
+                console.log("Modal has been onDismiss.");
+            }}
+                onShow={() => {
+                console.log("Modal has been onShow.");
+            }}
+                presentationStyle={"fullScreen"}>
+                <WebView
+                    source={{
+                    uri: `${ipAddress}/api/txn/paypal`,
+                    method: "post",
+                    body: `x-auth-token=${authToken}&ORDER_ID=${Date
+                        .now()
+                        .toString()}&CUST_ID=p2@gmail.com&TXN_AMOUNT=1&CALLBACK_URL=${ipAddress}/api/txn/paypal/status`
+                }}
+                    ref={webview => {
+                    this.webview = webview;
+                }}
+                    onError={this._onloadingError}
+                    onNavigationStateChange={data => {
+                    this._handleTransaction(data);
+                }}
+                    renderError={this._onloadingError}/>
+            </Modal>
+        );
     }
-  };
-  _onRequestClose = () => {
-    this.props.toggleModalVisiblity(false);
-  };
-  render() {
-    const { authToken, modalVisible } = this.props;
-    return (
-      <Modal
-        animationType="slide"
-        transparent={false}
-        visible={modalVisible}
-        onRequestClose={this._onRequestClose}
-        onDismiss={() => {
-          console.log("Modal has been onDismiss.");
-        }}
-        onShow={() => {
-          console.log("Modal has been onShow.");
-        }}
-        presentationStyle={"fullScreen"}
-      >
-        <WebView
-          source={{
-            uri: `${ipAddress}/api/txn/paypal`,
-            method: "post",
-            body: `x-auth-token=${authToken}&ORDER_ID=${Date.now().toString()}&CUST_ID=p2@gmail.com&TXN_AMOUNT=1&CALLBACK_URL=${ipAddress}/api/txn/paypal/status`
-          }}
-          ref={webview => {
-            this.webview = webview;
-          }}
-          onError={this._onloadingError}
-          onNavigationStateChange={data => {
-            this._handleTransaction(data);
-          }}
-          renderError={this._onloadingError}
-        />
-      </Modal>
-    );
-  }
 }
 
 export default PaytmPaymentModal;
--- components/RazorpayPaymentModal.js
+++ components/RazorpayPaymentModal.js
@@ -1,61 +1,68 @@
 import React from "react";
-import { Modal, WebView } from "react-native";
+import {Modal, WebView} from "react-native";
 import _ from "lodash";
-import { ipAddress } from "../constants";
+import {ipAddress} from "../constants";
 class RazorpayPaymentModal extends React.Component {
-  _onloadingError = x => {
-    console.log("_onloadingError");
-    this.props.toggleModalVisiblity(false);
-  };
-  _handleTransaction = data => {
-    const { loading, url, title } = data;
-    console.log(data);
-    if (
-      loading === false &&
-      _.startsWith(url, `${ipAddress}/api/txn/paypal/status`)
-    ) {
-      const jsonData = JSON.parse(title);
-      console.log("jsonData", jsonData);
-      this.props.toggleModalVisiblity(false);
+    _onloadingError = x => {
+        console.log("_onloadingError");
+        this
+            .props
+            .toggleModalVisiblity(false);
+    };
+    _handleTransaction = data => {
+        const {loading, url, title} = data;
+        console.log(data);
+        console.log(ipAddress);
+        if (loading === false && _.startsWith(url, `${ipAddress}/api/txn/razorpay/status`)) {
+            const jsonData = JSON.parse(title);
+            console.log("jsonData", jsonData);
+            this
+                .props
+                .toggleModalVisiblity(false);
+            this
+                .props
+                .getProfile();
+        }
+    };
+    _onRequestClose = () => {
+        this
+            .props
+            .toggleModalVisiblity(false);
+    };
+    render() {
+        const {authToken, modalVisible, txnAmount} = this.props;
+        return (
+            <Modal
+                animationType="slide"
+                transparent={false}
+                visible={modalVisible}
+                onRequestClose={this._onRequestClose}
+                onDismiss={() => {
+                console.log("Modal has been onDismiss.");
+            }}
+                onShow={() => {
+                console.log("Modal has been onShow.");
+            }}
+                presentationStyle={"fullScreen"}>
+                <WebView
+                    source={{
+                    uri: `${ipAddress}/api/txn/razorpay`,
+                    method: "post",
+                    body: `x-auth-token=${authToken}&ORDER_ID=${Date
+                        .now()
+                        .toString()}&CUST_ID=p2@gmail.com&TXN_AMOUNT=${txnAmount}&CALLBACK_URL=${ipAddress}/api/txn/razorpay/status`
+                }}
+                    ref={webview => {
+                    this.webview = webview;
+                }}
+                    onError={this._onloadingError}
+                    onNavigationStateChange={data => {
+                    this._handleTransaction(data);
+                }}
+                    renderError={this._onloadingError}/>
+            </Modal>
+        );
     }
-  };
-  _onRequestClose = () => {
-    this.props.toggleModalVisiblity(false);
-  };
-  render() {
-    const { authToken, modalVisible } = this.props;
-    return (
-      <Modal
-        animationType="slide"
-        transparent={false}
-        visible={modalVisible}
-        onRequestClose={this._onRequestClose}
-        onDismiss={() => {
-          console.log("Modal has been onDismiss.");
-        }}
-        onShow={() => {
-          console.log("Modal has been onShow.");
-        }}
-        presentationStyle={"fullScreen"}
-      >
-        <WebView
-          source={{
-            uri: `${ipAddress}/api/txn/razorpay`,
-            method: "get",
-            body: `x-auth-token=${authToken}&ORDER_ID=${Date.now().toString()}&CUST_ID=p2@gmail.com&TXN_AMOUNT=1&CALLBACK_URL=${ipAddress}/api/txn/paypal/status`
-          }}
-          ref={webview => {
-            this.webview = webview;
-          }}
-          onError={this._onloadingError}
-          onNavigationStateChange={data => {
-            this._handleTransaction(data);
-          }}
-          renderError={this._onloadingError}
-        />
-      </Modal>
-    );
-  }
 }
 
 export default RazorpayPaymentModal;
