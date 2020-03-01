160ee0e6bade0be9460b02e5b35eb183f05814e4
Prateek Agarwal <prateek.agarwal.30@gmail.com>
2020-03-01 19:49:14 +0530

    Working on Home Screen Components

--- components/MealListItems.js
+++ components/MealListItems.js
@@ -6,61 +6,68 @@ import QuantityBox from "./QuantityBox";
 const { width } = Dimensions.get("screen");
 export const MealsListItem = ({ mealData }) => {
   return (
-    <Animatable.View animation="fadeIn" iterationCount={1}>
-      <TouchableHighlight
-        onPress={() => {
-          console.log(mealData._id);
-        }}
-        underlayColor={"#E1E0E2"}
-        style={{
-          borderRadius: 10,
-          backgroundColor: "white",
-          marginVertical: 2.5,
-          marginHorizontal: 2.5,
-          width: width / 2 - 5
-        }}
-      >
-        <View>
-          <Animatable.Image
-            source={{ uri: `${ipAddress}/${mealData.mealImageUrl}` }}
-            style={{ height: 125, width: "100%", borderRadius: 10 }}
-            iterationCount={1}
-            animation={"fadeIn"}
-          />
-          <Text
-            style={{
-              color: "grey",
-              fontSize: 13,
-              marginLeft: 10,
-              fontWeight: "bold"
-            }}
-          >
-            {mealData.name}
-          </Text>
-          <View
-            style={{
-              flex: 1,
-              flexDirection: "row",
-              justifyContent: "space-between",
-              marginHorizontal: 5,
-              alignItems: "center",
-              marginVertical: 10
-            }}
-          >
+    <View
+      style={{
+        backgroundColor: "white",
+        marginVertical: 2.5,
+        marginHorizontal: 2.5,
+        borderRadius: 10
+      }}
+    >
+      <Animatable.View animation="fadeIn" iterationCount={1}>
+        <TouchableHighlight
+          onPress={() => {
+            console.log(mealData._id);
+          }}
+          underlayColor={"#E1E0E2"}
+          style={{
+            borderRadius: 10,
+            width: width / 2 - 5
+          }}
+        >
+          <View>
+            <Animatable.Image
+              source={{ uri: `${ipAddress}/${mealData.mealImageUrl}` }}
+              style={{ height: 125, width: "100%", borderRadius: 10 }}
+              iterationCount={1}
+              animation={"fadeIn"}
+            />
             <Text
               style={{
                 color: "grey",
-                fontSize: 18,
+                fontSize: 13,
                 marginLeft: 10,
                 fontWeight: "bold"
               }}
             >
-              {mealData.price} ₹
+              {mealData.name}
             </Text>
-            <QuantityBox />
           </View>
+        </TouchableHighlight>
+        {/* <View> */}
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginHorizontal: 5,
+            alignItems: "center",
+            marginVertical: 5
+          }}
+        >
+          <Text
+            style={{
+              color: "grey",
+              fontSize: 18,
+              marginLeft: 10,
+              fontWeight: "bold"
+            }}
+          >
+            {mealData.price} ₹
+          </Text>
+          <QuantityBox />
         </View>
-      </TouchableHighlight>
-    </Animatable.View>
+      </Animatable.View>
+    </View>
   );
 };
--- components/QuantityBox.js
+++ components/QuantityBox.js
@@ -1,43 +1,88 @@
-import { Text, View } from "react-native";
+import { Text, View, Alert } from "react-native";
 import { Button } from "native-base";
 import React from "react";
-export default props => {
-  return (
-    <View
-      style={{
-        flex: 7,
-        flexDirection: "row",
-        height: 30,
-        borderColor: "#E19D40",
-        borderWidth: 1.5,
-        maxWidth: 60,
-        borderRadius: 5
-      }}
-    >
-      <Button
-        transparent
-        style={{ flex: 2, height: 23, justifyContent: "center" }}
+import * as Animatable from "react-native-animatable";
+export default class QuantityBox extends React.Component {
+  state = {
+    quantity: 0
+  };
+  handleQty = x => {
+    // console.log(x);
+    if (x === "-" && this.state.quantity !== 0) {
+      this.setState(prevState => ({
+        ...prevState,
+        quantity: prevState.quantity - 1
+      }));
+    } else if (x === "+") {
+      if (this.state.quantity >= 5) {
+        Alert.alert("Max 5 same meals can be ordered in a order.");
+      } else {
+        this.setState(prevState => ({
+          ...prevState,
+          quantity: prevState.quantity + 1
+        }));
+      }
+    }
+  };
+  render() {
+    return (
+      <View
+        style={{
+          flex: 7,
+          flexDirection: "row",
+          height: 30,
+          borderColor: "#E19D40",
+          borderWidth: 1.5,
+          maxWidth: 70,
+          borderRadius: 5,
+          backgroundColor: "white"
+        }}
       >
-        <Text style={{ color: "#E19D40", fontSize: 24, fontWeight: "bold" }}>
-          -
-        </Text>
-      </Button>
-      <Button
-        transparent
-        style={{ flex: 3, height: 25, justifyContent: "center" }}
-      >
-        <Text style={{ color: "#E19D40", fontSize: 16, fontWeight: "bold" }}>
-          0
-        </Text>
-      </Button>
-      <Button
-        transparent
-        style={{ flex: 2, height: 23, justifyContent: "center" }}
-      >
-        <Text style={{ color: "#E19D40", fontSize: 18, fontWeight: "bold" }}>
-          +
-        </Text>
-      </Button>
-    </View>
-  );
-};
+        <Button
+          transparent
+          style={{ flex: 2, height: 25, justifyContent: "center" }}
+          onPress={() => this.handleQty("-")}
+        >
+          <Text
+            style={{
+              color: this.state.quantity === 0 ? "grey" : "#E19D40",
+              fontSize: 16,
+              fontWeight: "bold"
+            }}
+          >
+            -
+          </Text>
+        </Button>
+        <Animatable.View
+          animation={"fadeIn"}
+          style={{
+            flex: 3,
+            height: 26.5,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#E19D40"
+          }}
+        >
+          <Text style={{ color: "white", fontSize: 16, fontWeight: "bold" }}>
+            {this.state.quantity}
+          </Text>
+        </Animatable.View>
+        <Button
+          transparent
+          style={{ flex: 2, height: 25, justifyContent: "center" }}
+          onPress={() => this.handleQty("+")}
+        >
+          <Text
+            style={{
+              color: this.state.quantity === 5 ? "grey" : "#E19D40",
+              fontSize: 16,
+              fontWeight: "bold"
+            }}
+          >
+            +
+          </Text>
+        </Button>
+      </View>
+    );
+  }
+}
