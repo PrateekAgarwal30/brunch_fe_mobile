15590d47ae70ead289b9c4022ab3ef585a12e662
Prateek Agarwal <prateek.agarwal.30@gmail.com>
2020-03-08 22:05:47 +0530

    Working on Cart SCreen components

---
+++ components/CartListItems.js
@@ -0,0 +1,215 @@
+import React from "react";
+import * as Animatable from "react-native-animatable";
+import { Text, View, Dimensions } from "react-native";
+import { ipAddress } from "../constants";
+import QuantityBox from "./QuantityBox";
+import {
+  Placeholder,
+  PlaceholderMedia,
+  PlaceholderLine,
+  ShineOverlay
+} from "rn-placeholder";
+const { width } = Dimensions.get("screen");
+export const CartListItem = ({ mealData }) => {
+  console.log(mealData);
+  return (
+    <View
+      style={{
+        backgroundColor: "white",
+        marginVertical: 2.5,
+        marginHorizontal: 7.5,
+        borderRadius: 10
+      }}
+    >
+      <Animatable.View
+        animation="fadeIn"
+        iterationCount={1}
+        style={{ flexDirection: "row", alignItems: "center" }}
+      >
+        <Animatable.Image
+          source={{ uri: `${ipAddress}/${mealData.mealThumbnailUrl}` }}
+          style={{ height: 75, width: 105, borderRadius: 10 }}
+          iterationCount={1}
+          animation={"fadeIn"}
+        />
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "space-between",
+            marginHorizontal: 5,
+            marginVertical: 5
+          }}
+        >
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "row",
+              maxWidth: 195,
+              alignSelf: "flex-start"
+            }}
+          >
+            <Text
+              style={{
+                color: "grey",
+                fontSize: 16,
+                marginLeft: 10,
+                fontWeight: "bold",
+                width: "90%"
+              }}
+            >
+              {mealData.name}
+            </Text>
+            <Text
+              style={{
+                color: "grey",
+                fontSize: 12,
+                marginLeft: 10,
+                fontWeight: "bold",
+                alignSelf: "center"
+              }}
+            >
+              {`${mealData.price} ₹`}
+            </Text>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "row",
+              justifyContent: "space-between",
+              marginHorizontal: 5,
+              marginVertical: 5
+            }}
+          >
+            <Text
+              style={{
+                color: "grey",
+                fontSize: 12,
+                marginLeft: 10,
+                fontWeight: "bold",
+                textAlignVertical: "center"
+              }}
+            >
+              {`Item Total:`}
+            </Text>
+            <Text
+              style={{
+                color: "grey",
+                fontSize: 16,
+                marginLeft: 10,
+                fontWeight: "bold",
+                textAlignVertical: "center"
+              }}
+            >
+              {`${mealData.itemTotalPrice} ₹`}
+            </Text>
+            <QuantityBox mealId={mealData._id} quantity={mealData.quantity} />
+          </View>
+        </View>
+      </Animatable.View>
+    </View>
+  );
+};
+
+export const NoCartListItem = () => {
+  return (
+    <View>
+      <Placeholder
+        Animation={ShineOverlay}
+        style={{
+          backgroundColor: "white",
+          width: width / 2,
+          // height: 200,
+          flex: 1
+        }}
+      >
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          <View style={{ margin: 2.5 }}>
+            <PlaceholderMedia
+              size={width / 2 - 15}
+              style={{ height: 125, margin: 5 }}
+            />
+            <PlaceholderLine />
+            <View
+              style={{
+                flex: 1,
+                flexDirection: "row",
+                justifyContent: "space-between",
+                marginHorizontal: 10
+              }}
+            >
+              <PlaceholderLine width={20} height={20} />
+              <PlaceholderLine width={20} height={20} />
+            </View>
+          </View>
+          <View style={{ margin: 2.5 }}>
+            <PlaceholderMedia
+              size={width / 2 - 15}
+              style={{ height: 125, margin: 5 }}
+            />
+            <PlaceholderLine />
+            <View
+              style={{
+                flex: 1,
+                flexDirection: "row",
+                justifyContent: "space-between",
+                marginHorizontal: 10
+              }}
+            >
+              <PlaceholderLine width={20} height={20} />
+              <PlaceholderLine width={20} height={20} />
+            </View>
+          </View>
+        </View>
+      </Placeholder>
+      <Placeholder
+        Animation={ShineOverlay}
+        style={{
+          backgroundColor: "white",
+          width: width / 2,
+          // height: 200,
+          flex: 1
+        }}
+      >
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          <View style={{ margin: 2.5 }}>
+            <PlaceholderMedia
+              size={width / 2 - 15}
+              style={{ height: 125, margin: 5 }}
+            />
+            <PlaceholderLine />
+            <View
+              style={{
+                flex: 1,
+                flexDirection: "row",
+                justifyContent: "space-between",
+                marginHorizontal: 10
+              }}
+            >
+              <PlaceholderLine width={20} height={20} />
+              <PlaceholderLine width={20} height={20} />
+            </View>
+          </View>
+          <View style={{ margin: 2.5 }}>
+            <PlaceholderMedia
+              size={width / 2 - 15}
+              style={{ height: 125, margin: 5 }}
+            />
+            <PlaceholderLine />
+            <View
+              style={{
+                flex: 1,
+                flexDirection: "row",
+                justifyContent: "space-between",
+                marginHorizontal: 10
+              }}
+            >
+              <PlaceholderLine width={20} height={20} />
+              <PlaceholderLine width={20} height={20} />
+            </View>
+          </View>
+        </View>
+      </Placeholder>
+    </View>
+  );
+};
--- screens/Cart.js
+++ screens/Cart.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { View, AsyncStorage, FlatList } from "react-native";
+import { Text, View, AsyncStorage, FlatList } from "react-native";
 import { connect } from "react-redux";
+import { LinearGradient } from "expo-linear-gradient";
+import { Header, Body, Button, Icon, Left, Right } from "native-base";
 import { withAppContextConsumer } from "../components/AppContext";
 import { postCartItems, clearCartItems } from "../redux/actions";
-import { MealsListItem, NoMealsListItem } from "../components/MealListItems";
+import { CartListItem, NoCartListItem } from "../components/CartListItems";
 import appEventEmitter from "../utils/eventUtil";
+import _ from "lodash";
 class Cart extends React.Component {
   constructor(props) {
     super(props);
@@ -37,22 +40,72 @@ class Cart extends React.Component {
   }
 
   render() {
+    const { themes } = this.props;
     const { cart, isCartLoading } = this.props.user;
-    // console.log(cart);
+    const cartData = _.get(cart, "cartItems", []) || [];
     return (
-      <View>
+      <View
+        style={{
+          flex: 1,
+          zIndex: 0,
+          backgroundColor: "#EDEEF1"
+        }}
+      >
+        <LinearGradient
+          colors={[themes["light"].secondary, themes["light"].primary]}
+          style={{
+            borderBottomLeftRadius: 25,
+            borderBottomRightRadius: 25,
+            elevation: 2
+          }}
+        >
+          <Header transparent>
+            <Left>
+              <Button
+                transparent
+                onPress={() => this.props.navigation.goBack()}
+              >
+                <Icon
+                  name="arrow-back"
+                  style={{ color: "white", fontSize: 25 }}
+                />
+              </Button>
+            </Left>
+            <Body>
+              <Text
+                style={{ fontSize: 20, color: "white", fontWeight: "bold" }}
+              >
+                Cart
+              </Text>
+            </Body>
+            <Right />
+          </Header>
+        </LinearGradient>
         <FlatList
-          data={cart ? cart.cartItems || [] : []}
-          renderItem={({ item }) => <MealsListItem mealData={item} />}
+          data={cartData}
+          renderItem={({ item }) => <CartListItem mealData={item} />}
           keyExtractor={itemData => itemData._id}
           onRefresh={() => {
             this.props.postCartItems();
           }}
           refreshing={isCartLoading || false}
-          // ListHeaderComponent={<BannerCarousel />}
-          numColumns={2}
-          ListEmptyComponent={<NoMealsListItem />}
+          ListEmptyComponent={<NoCartListItem />}
         />
+        {cartData.length ? (
+          <Button
+            style={{
+              textAlign: "center",
+              justifyContent: "center",
+              margin: 10,
+              backgroundColor: themes["light"].primary
+            }}
+            onPress={this._onChangeAddress}
+          >
+            <Text style={{ color: "white", fontSize: 16 }}>
+              Proceed to Payment
+            </Text>
+          </Button>
+        ) : null}
       </View>
     );
   }
